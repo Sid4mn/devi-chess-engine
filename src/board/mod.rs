51232e7ee53d07966b76ef378c97562beb1eee95
sid4mn <siddhantshettiwar@gmail.com>
2025-07-14 00:18:25 -0600

    Implemented board rep, starting position setup

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -32,4 +32,33 @@ impl Board {
     pub fn is_empty(&self, square: Square) -> bool {
         self.squares[square.0 as usize].is_none()
     }
+
+    pub fn setup_starting_position(&mut self) {
+        self.set_piece(Square(0), Some(Piece::new(PieceType::Rook, Color::White))); //a1
+        self.set_piece(Square(1), Some(Piece::new(PieceType::Knight, Color::White))); //b1
+        self.set_piece(Square(2), Some(Piece::new(PieceType::Bishop, Color::White))); //c1
+        self.set_piece(Square(3), Some(Piece::new(PieceType::Queen, Color::White))); //d1
+        self.set_piece(Square(4), Some(Piece::new(PieceType::King, Color::White))); //e1
+        self.set_piece(Square(5), Some(Piece::new(PieceType::Bishop, Color::White))); //f1
+        self.set_piece(Square(6), Some(Piece::new(PieceType::Knight, Color::White))); //g1
+        self.set_piece(Square(7), Some(Piece::new(PieceType::Rook, Color::White))); //h1
+
+        for i in 8..16 { // 2
+            self.set_piece(Square(i), Some(Piece::new(PieceType::Pawn, Color::White))); 
+        }
+
+        for i in 48..56 { // 7
+            self.set_piece(Square(i), Some(Piece::new(PieceType::Pawn, Color::Black))); 
+        }
+
+        self.set_piece(Square(56), Some(Piece::new(PieceType::Rook, Color::Black))); //a8
+        self.set_piece(Square(57), Some(Piece::new(PieceType::Knight, Color::Black))); //b8
+        self.set_piece(Square(58), Some(Piece::new(PieceType::Bishop, Color::Black))); //c8
+        self.set_piece(Square(59), Some(Piece::new(PieceType::Queen, Color::Black))); //d8
+        self.set_piece(Square(60), Some(Piece::new(PieceType::King, Color::Black))); //e8
+        self.set_piece(Square(61), Some(Piece::new(PieceType::Bishop, Color::Black))); //f8
+        self.set_piece(Square(62), Some(Piece::new(PieceType::Knight, Color::Black))); //g8
+        self.set_piece(Square(63), Some(Piece::new(PieceType::Rook, Color::Black))); //h8
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,12 @@ fn main() {
     println!("devi Chess Engine v0.1.0");
     println!("------------------------");
 
-
     let mut board = Board::new();    
-    let white_pawn = Piece::new(PieceType::Pawn, Color::White);
+    board.setup_starting_position();
+    // let white_pawn = Piece::new(PieceType::Pawn, Color::White);
+    // board.set_piece(Square(8), Some(white_pawn));
 
-    board.set_piece(Square(8), Some(white_pawn));
-
+    //starting positions
     println!("Piece at a2: {:?}", board.get_piece(Square(8)));
     println!("e4 is empty? : {:?}", board.is_empty(Square(28)));
-
-
 }
